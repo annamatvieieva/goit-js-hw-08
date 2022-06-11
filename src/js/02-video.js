bdfc8a0afd818320986aa69f3bc28b8681ec192c
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2022-06-11 21:06:40 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,49 @@
-import Vimeo  from "@vimeo/player";
+import Vimeo from "@vimeo/player";
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
+const localStorageKey = "videoplayer-current-time";
+const lastPlaybackTime = JSON.parse(localStorage.getItem(localStorageKey));
+let gCurrentTime = {seconds: 0};
+const throttling = throttle(() => {
+	localStorage.setItem(localStorageKey, gCurrentTime.seconds || 0)
+}, 1000);
 
+checklastPlaybackTime(lastPlaybackTime);
+
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+});
 player.on('play', function() {
         console.log('played the video!');
     });
-player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.on('timeupdate', function (currentTime) {
+	gCurrentTime = currentTime;
+	throttling();
+});
+function checklastPlaybackTime (time) {
+	if (time) {
+		console.log("Вы остановились на", time, "сек.");
+		player.setCurrentTime(time);
+	} else {
+		console.log("Вы еще не смотрели это видео");
+	}
+}
 
-player.on('timeupdate', function(currentTime) {
-	console.log('video timeupdate:', currentTime);
-    });
+
+// const a = localStorage.setItem;
+// const b = 2;
+
+// function four(c, d) {
+// 	let a1 = c || a;
+// 	// let b1 = d || b;
+
+// 	a1()
+
+// 	// return a1 + b1;
+// }
+
+// console.log('four', four());
+// console.log('four', four(3));
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
 
+
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('input[type="email"]');
+const textareaMessage = document.querySelector('textarea[name="message"]');
+const localStorageKey = "feedback-form-state";
+const lastUserData = JSON.parse(localStorage.getItem(localStorageKey));
+
+
+
+console.log('lastUserData', lastUserData);
+
+form.addEventListener('input', () => {
+	const  userEmail = event.currentTarget.elements.email.value;
+	const userMessage = event.currentTarget.elements.message.value;
+	const userData = {
+		email: userEmail,
+		message: userMessage
+	};
+	const userDataJSON = JSON.stringify(userData);
+	console.log('userDataJSON', userDataJSON);
+	localStorage.setItem(localStorageKey, userDataJSON);
+})
+

073b6524270d330f5207928effb5148713bc0b11
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2022-06-12 09:36:34 +0300

    add task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,49 +1,29 @@
-import Vimeo from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
-const localStorageKey = "videoplayer-current-time";
+const localStorageKey = 'videoplayer-current-time';
 const lastPlaybackTime = JSON.parse(localStorage.getItem(localStorageKey));
-let gCurrentTime = {seconds: 0};
+let gCurrentTime = { seconds: 0 };
 const throttling = throttle(() => {
-	localStorage.setItem(localStorageKey, gCurrentTime.seconds || 0)
+  localStorage.setItem(localStorageKey, Math.floor(gCurrentTime.seconds));
 }, 1000);
 
-checklastPlaybackTime(lastPlaybackTime);
+getlastPlaybackTime(lastPlaybackTime);
+savePlayTimeLS();
 
-player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-});
-player.on('play', function() {
-        console.log('played the video!');
-    });
-player.on('timeupdate', function (currentTime) {
-	gCurrentTime = currentTime;
-	throttling();
-});
-function checklastPlaybackTime (time) {
-	if (time) {
-		console.log("Вы остановились на", time, "сек.");
-		player.setCurrentTime(time);
-	} else {
-		console.log("Вы еще не смотрели это видео");
-	}
+function savePlayTimeLS() {
+  player.on('timeupdate', function (currentTime) {
+    gCurrentTime = currentTime;
+    throttling();
+  });
 }
-
-
-// const a = localStorage.setItem;
-// const b = 2;
-
-// function four(c, d) {
-// 	let a1 = c || a;
-// 	// let b1 = d || b;
-
-// 	a1()
-
-// 	// return a1 + b1;
-// }
-
-// console.log('four', four());
-// console.log('four', four(3));
-
+function getlastPlaybackTime(time) {
+  if (time) {
+    console.log('Вы остановились на', time, 'сек.');
+    player.setCurrentTime(time);
+  } else {
+    console.log('Вы еще не смотрели это видео');
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,54 @@
-
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const button = document.querySelector('button[type="submit"]');
 const inputEmail = document.querySelector('input[type="email"]');
 const textareaMessage = document.querySelector('textarea[name="message"]');
-const localStorageKey = "feedback-form-state";
+const localStorageKey = 'feedback-form-state';
 const lastUserData = JSON.parse(localStorage.getItem(localStorageKey));
 
+getlastUserData(lastUserData);
+saveUserDataLS();
+clearUserDataLS();
+
+function saveUserDataLS() {
+  form.addEventListener('input', throttle(() => {
+    const userEmail = event.currentTarget.elements.email.value;
+    const userMessage = event.currentTarget.elements.message.value;
+    const userData = {
+      email: userEmail,
+      message: userMessage,
+    };
+    const userDataJSON = JSON.stringify(userData);
+    localStorage.setItem(localStorageKey, userDataJSON);
+  }, 500));
+}
+function clearUserDataLS() {
+  form.addEventListener('submit', () => {
+    event.preventDefault();
+    const subUserEmail = event.target.elements.email.value;
+    const subUserMessage = event.target.elements.message.value;
+    const subUserData = {
+      email: subUserEmail,
+      message: subUserMessage,
+    };
+    console.log(subUserData);
+    localStorage.removeItem(localStorageKey);
+    inputEmail.value = '';
+    textareaMessage.value = '';
+  });
+}
+function getlastUserData(data) {
+  if (data) {
+    inputEmail.value = lastUserData.email;
+    textareaMessage.value = lastUserData.message;
+  }
+}
 
 
-console.log('lastUserData', lastUserData);
-
-form.addEventListener('input', () => {
-	const  userEmail = event.currentTarget.elements.email.value;
-	const userMessage = event.currentTarget.elements.message.value;
-	const userData = {
-		email: userEmail,
-		message: userMessage
-	};
-	const userDataJSON = JSON.stringify(userData);
-	console.log('userDataJSON', userDataJSON);
-	localStorage.setItem(localStorageKey, userDataJSON);
-})
-
+// try {
+//   const data = JSON.parse('{username: "Mango"}');
+// } catch (error) {
+//   console.log(error.name); // "SyntaxError"
+//   console.log(error.message); // "Unexpected token u in JSON at position 1"
+// }
